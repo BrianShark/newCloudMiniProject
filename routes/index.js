fc502096d293a323ab90f3cb35af16f344d3b375
Brian Sharkey <briansharkey07@gmail.com>
2021-04-18 12:43:35 +0100

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var Time = require('../models/time');
+var mongoose = require('mongoose');
+/* GET home page. */
+mongoose.connect("mongodb://localhost:27017/timestamps", { useNewUrlParser: true });
+router.get('/', function(req, res, next) {
+Time.find(function(err,docs){
+    var timeChunks = [];
+    chunkSize = 3;
+    for(var i = 0; i < docs.length; i+= chunkSize ){
+      timeChunks.push(docs.slice(i,i+chunkSize));
+    }
+    res.render('home/index', { title: 'Cloud Security System', times: timeChunks });
+  }).lean();
+
+
+});
+
+module.exports = router;
